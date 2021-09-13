3e2d924d0bff60e86cd106a9c18240ad040dcd6b
pasanca7 <pabdecar@alum.us.es>
2021-09-13 10:33:53 +0200

    Fix of image validation

--- Gymkana/portal/urls.py
+++ Gymkana/portal/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'portal'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name = 'index.html'),
+    path('', views.IndexView.as_view(), name = 'index'),
     path('v1/news/create', views.create_new, name = 'new_form'),
     path('v1/news/<int:new_id>', views.read_new, name = 'read_new'),
+    path('v1/news/<int:new_id>/edit', views.edit_new, name = 'edit_new'),
     ]
--- Gymkana/portal/views.py
+++ Gymkana/portal/views.py
@@ -1,7 +1,4 @@
-import os
 import io
-
-from django.forms.utils import ErrorList
 
 from portal.forms import NewForm
 from portal.models import Event, New
@@ -46,14 +43,28 @@ def create_new(request):
         new_form = NewForm(request.POST, request.FILES)        
         if new_form.is_valid():
             new_form.save()
-            return redirect('/portal/')
+            return redirect('portal:index')
     else:
         new_form = NewForm()
     return render(request, 'portal/new_form.html', {'new_form':new_form})
 
 """
-Read of new in FBV
+Read new in FBV
 """
 def read_new(request, new_id):
     new = get_object_or_404(New, pk = new_id)
     return render(request, 'portal/read_new.html', {'new': new})
+
+"""
+Edit new in FBV
+"""
+def edit_new(request, new_id):
+    new = get_object_or_404(New, pk = new_id)
+    if request.method == 'POST':
+        form = NewForm(request.POST or None, files =  request.FILES or None, instance = new)
+        if form.is_valid:
+            form.save()
+            return redirect('')
+    else:
+        form = NewForm(instance = new, initial={'new_id': new_id})
+    return render(request, 'portal/edit_new.html', {'form':form})
