1d244f2692c8b4062eed9a19d6617c1eccd75449
pasanca7 <pabdecar@alum.us.es>
2021-09-14 12:07:38 +0200

    Read news tests

--- Gymkana/portal/tests.py
+++ Gymkana/portal/tests.py
@@ -14,7 +14,7 @@ from django.shortcuts import reverse
 class index_tests(TestCase):
 
     def setUp(self):
-        n1 = New.objects.create(title="Título 1", subtitle="Subtítulo 1", body="Cuerpo 1")
+        New.objects.create(title="Título 1", subtitle="Subtítulo 1", body="Cuerpo 1")
         New.objects.create(title="Título 2", subtitle="Subtítulo 2", body="Cuerpo 2")
         New.objects.create(title="Título 3", subtitle="Subtítulo 3", body="Cuerpo 3")
 
@@ -40,5 +40,34 @@ class index_tests(TestCase):
         self.assertQuerysetEqual(Event.objects.filter(title='Título 2'), ['<Event: Event object (2)>'])
         self.assertQuerysetEqual(Event.objects.filter(title='Título 3'), ['<Event: Event object (3)>'])
 
+class new_creation_tests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_creation_new_page(self):
+        url = reverse('portal:new_form')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'portal/new_form.html')
+
+    def test_create_new(self):
+        url = reverse('portal:new_form')
+        new_content = {'title':'Título 4', 'subtitle':'Test #5', 'body':'Esta es una noticia de prueba.'}
+        response = self.client.post(url, new_content)
+        self.assertEqual(response.status_code, 302)
+        self.assertQuerysetEqual(New.objects.filter(title='Título 4'), ['<New: New object (1)>'])
+
+class new_read_tests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        New.objects.create(title="Título 5", subtitle="Subtítulo 5", body="Cuerpo 5")
+
+    def test_read_new(self):
+        url = reverse('portal:read_new', kwargs={'new_id': 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'portal/read_new.html')
 
 
