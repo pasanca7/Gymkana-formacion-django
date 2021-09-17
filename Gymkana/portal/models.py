af9ebd8e7ea618a85dbdcb50823aa6da3cd49823
pasanca7 <pabdecar@alum.us.es>
2021-09-17 10:46:54 +0200

    Change image name when upload

--- Gymkana/portal/models.py
+++ Gymkana/portal/models.py
@@ -1,5 +1,17 @@
+import os
+
 from django.db import models
 from django.utils import timezone
+from uuid import uuid4
+from datetime import date
+
+def path_and_rename(instance, filename):
+        upload_to = ""
+        name = filename.split('.')[0]
+        ext = filename.split('.')[-1]
+        now = instance.publish_date.strftime("%d-%m-%YT%H%M")
+        filename = '{}{}.{}'.format(name, now, ext)
+        return os.path.join(upload_to, filename)
 
 class BaseItem(models.Model):
     title = models.CharField(max_length = 100, blank = False)
@@ -11,7 +23,7 @@ class BaseItem(models.Model):
 
 class New(BaseItem):
     publish_date = models.DateTimeField(default = timezone.now)
-    image = models.ImageField(upload_to = '', default ='default.jpg')
+    image = models.ImageField(upload_to=path_and_rename, default ='default.jpg')
 
 class Event(BaseItem):
     start_date = models.DateTimeField(null=False)
